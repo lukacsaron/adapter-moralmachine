d1a6288f263a503b3412e0b982133f65388e7712
Aron Lukacs <aron.lukacs@flashbirdcreative.com>
2023-08-31 14:03:15 +0200

    version increase

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var version = "v3" // increase for new version
+var version = "v4" // increase for new version
 var staticCacheName = version + "_pwa-static";
 var dynamicCacheName = version + "_pwa-dynamic";
 
